f42b08d185a9360bb8f7714d79d7fccefe1b6bcd
Maksim Voronkov <joukmaks@yandex.ru>
2023-05-07 21:57:58 +0400

    Create MeshUtils.cs

---
+++ Voronkovmm/MeshUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Voronkovmm
+{
+    public static class MeshUtils
+    {
+        public static void CreateEmptyMeshArrays(int quadCount, out Vector3[] vertices, out Vector2[] uvs, out int[] triangles)
+        {
+            vertices = new Vector3[4 * quadCount];
+            uvs = new Vector2[4 * quadCount];
+            triangles = new int[6 * quadCount];
+        }
+    } 
+}
+
