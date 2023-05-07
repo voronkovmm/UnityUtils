ba2eb29df47aabea8319328e1c312303e2a96ed1
Maksim Voronkov <joukmaks@yandex.ru>
2023-05-07 20:14:57 +0400

    Create WorldText, Helper

---
+++ Voronkovmm/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Voronkovmm
+{
+    public static class Helper
+    {
+        public static T With<T>(this T self, Action<T> set)
+        {
+            set.Invoke(self);
+            return self;
+        }
+    } 
+}
---
+++ Voronkovmm/WorldText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Voronkovmm
+{
+    public static class WorldText
+    {
+        public static TextMesh Create(string text, Transform parent, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+        {
+            GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+
+            Transform transform = gameObject.transform
+                .With(x => x.SetParent(parent, false))
+                .With(x => x.localPosition = localPosition);
+
+            TextMesh textMesh = gameObject.GetComponent<TextMesh>()
+                .With(x => x.anchor = textAnchor)
+                .With(x => x.alignment = textAlignment)
+                .With(x => x.text = text)
+                .With(x => x.fontSize = fontSize)
+                .With(x => x.color = color)
+                .With(x => x.GetComponent<MeshRenderer>().sortingOrder = sortingOrder);
+
+            return textMesh;
+        }
+    } 
+}
